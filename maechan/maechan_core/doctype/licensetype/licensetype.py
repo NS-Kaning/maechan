cda8758692e42801d488acbe1af0c6a33b7c9302
Chaow Porkaew <pchaowmobile@gmail.com>
2023-11-12 13:49:56 +0000

    add year

--- maechan/maechan_core/doctype/licensetype/licensetype.py
+++ maechan/maechan_core/doctype/licensetype/licensetype.py
@@ -6,4 +6,19 @@ from frappe.model.document import Document
 
 
 class LicenseType(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from maechan.maechan_core.doctype.licensetypedetail.licensetypedetail import LicenseTypeDetail
+
+		details: DF.Table[LicenseTypeDetail]
+		group: DF.Link
+		license_type: DF.Literal['ใบอนุญาต', 'หนังสือรับรองการแจ้ง']
+		rule_year: DF.Data | None
+		title: DF.Data
+	# end: auto-generated types
 	pass
