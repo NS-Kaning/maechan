153bfe19f32dc4d6dc7a72e49120761ed94b8efc
Chaow Porkaew <pchaowmobile@gmail.com>
2023-11-15 19:08:49 +0700

    add close/open watergate

---
+++ maechan/maechan_water/doctype/watergate/watergate_list.js
@@ -0,0 +1,78 @@
+let closed_button = {
+    show(doc) {
+        return doc.status === "เปิด";
+    },
+    get_label() {
+        return __("ปิด");
+    },
+    get_description(doc) {
+        return "ปิดประตูน้ำ"
+    },
+    action(doc) {
+        fetch(`/api/resource/WaterGate/${doc.name}`, {
+            method : 'PUT',
+            headers: { 'Content-Type': 'application/json' ,
+            "X-Frappe-CSRF-Token": frappe.csrf_token},
+            body : JSON.stringify({
+                "status" : "ปิด"
+            })
+        }).then(r=>console.log(r))
+    }
+}
+
+let open_button = {
+    show(doc) {
+        return doc.status === "ปิด";
+    },
+    get_label() {
+        return __("เปิด");
+    },
+    get_description(doc) {
+        return "เปิดประตูน้ำ"
+    },
+    action(doc) {
+        fetch(`/api/resource/WaterGate/${doc.name}`, {
+            method : 'PUT',
+            headers: { 'Content-Type': 'application/json' ,
+            "X-Frappe-CSRF-Token": frappe.csrf_token},
+            body : JSON.stringify({
+                "status" : "เปิด"
+            })
+        }).then(r=>console.log(r))
+    }
+}
+
+frappe.listview_settings['WaterGate'] = {
+    add_fields: ['title', 'status'],
+
+    has_indicator_for_draft: false,
+    get_indicator(doc) {
+        // customize indicator color
+        if (doc.status == "เปิด") {
+            return [__("เปิด"), "green",""];
+        } else if (doc.status =="ปิด") {
+            return [__("ปิด"), "darkgrey",""];
+        }
+    },
+    button : {
+        show(doc) {
+            return true
+        },
+        get_label() {
+            return __("เปิด/ปิด");
+        },
+        get_description(doc) {
+            return "เปิด/ปิด ประตูน้ำ"
+        },
+        action(doc) {
+            fetch(`/api/resource/WaterGate/${doc.name}`, {
+                method : 'PUT',
+                headers: { 'Content-Type': 'application/json' ,
+                "X-Frappe-CSRF-Token": frappe.csrf_token},
+                body : JSON.stringify({
+                    "status" : doc.status === "เปิด" ? "ปิด" : "เปิด"
+                })
+            }).then(r=>console.log(r))
+        }
+    }
+}
