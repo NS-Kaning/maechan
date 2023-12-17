b5fa24ab8b9d6ca0c2fc94fbd941ab9eb780856f
Chaow Porkaew <pchaowmobile@gmail.com>
2023-12-17 08:24:43 +0000

    remove commit

--- maechan/maechan_core/tasks.py
+++ maechan/maechan_core/tasks.py
@@ -8,12 +8,11 @@ def update_expired_license():
         'license_end_date': ['<',datetime.today()],
         'workflow_state' : ['!=','Expired']
         }
+    
     expired_license = frappe.db.get_list('License',fields=['name'],filters=filters)
     try :
         for i in expired_license :
             frappe.db.set_value('License',i.name,'workflow_state','Expired')
-            
-        frappe.db.commit()
     except :
         frappe.db.rollback(save_point='before_update_expired')
         
