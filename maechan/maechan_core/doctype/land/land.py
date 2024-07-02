fdd70743fd391b5baa50d202aa922fa25f84f68e
Chaow Porkaew <pchaowmobile@gmail.com>
2024-07-02 11:49:56 +0000

    update land

--- maechan/maechan_core/doctype/land/land.py
+++ maechan/maechan_core/doctype/land/land.py
@@ -6,6 +6,28 @@ from frappe.model.document import Document
 
 
 class Land(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        block_id: DF.Data | None
+        district_id: DF.Link | None
+        geojson: DF.JSON
+        land_owner: DF.Data | None
+        land_size: DF.Data | None
+        land_type: DF.Data | None
+        lat: DF.Float
+        lng: DF.Float
+        lot: DF.Data | None
+        parcel_cod: DF.Data | None
+        parcel_no: DF.Data | None
+        zone_id: DF.Data | None
+    # end: auto-generated types
+
     
     from typing import TYPE_CHECKING
 
